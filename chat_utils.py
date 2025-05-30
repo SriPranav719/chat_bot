44c9d36ea7d84974ae7217bd27f7f049b14e7fb3
unknown <sripranav5942@gmail.com>
2025-05-30 13:06:35 +0530

    Initial commit

--- chat_utils.py
+++ chat_utils.py
@@ -8,7 +8,7 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 async def get_gemini_response(user_message: str):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}" #base endpoint for gemini(API) to generate content, and automatically authorizes the API_KEY
 
     headers = {
         "Content-Type": "application/json"
@@ -17,18 +17,20 @@ async def get_gemini_response(user_message: str):
     payload = {
         "contents": [
             {
-                "parts": [{"text": user_message}]
+                "parts": [{"text" : user_message}]  # actual data where the request is stored.
             }
         ]
     }
 
-    async with httpx.AsyncClient(timeout=30) as client:
+    async with httpx.AsyncClient(timeout=30) as client: # timeout=30 so that if the API doesnt responds then it raises a timeout error.
         response = await client.post(url, headers=headers, json=payload)
-        response.raise_for_status()
-        data = response.json()
+        response.raise_for_status() # to catch the exceptions , if 200 OK the app runs flexible, otherwise it raises an exception httpx.HTTPStatusError
+        data = response.json()    
+        # converting the json to python Dict so that we can extract the data easily 
         try:
-            text = data["candidates"][0]["content"]["parts"][0]["text"]
-            return text
+            text = data["candidates"][0]["content"]["parts"][0]["text"] #accessing the prompt(actual response)
+            
+            return text  
         except Exception as e:
             print("Error parsing response:", e)
             return "Error parsing response"
--- main.py
+++ main.py
@@ -4,13 +4,28 @@ from chat_utils import get_gemini_response
 
 app = FastAPI()
 
+# Temporary memory storage
+chat_history = []  
+
 @app.post("/chat")
 async def chat(request: Request):
-    data = await request.json()
+    data = await request.json() #converting the input json message into python dict so that we can access the data
     user_message = data.get("message", "")
 
-    # Call the Gemini API and get full response (non-streaming)
-    reply = await get_gemini_response(user_message)
+    # Call the Gemini API
+    reply = await get_gemini_response(user_message) #calling the gemini_response to process the request and fetch the response 
 
-    # Return normal JSON response (no streaming here)
-    return JSONResponse(content={"reply": reply})
+    # appending the chat history in the list 
+    chat_history.append({
+        "user": user_message,
+        "bot": reply
+    })
+
+    return JSONResponse(content={"reply": reply}) # 
+
+
+#here creating a get request to get the chat history, but it gets deleted when the server restarts.
+@app.get("/history")
+def get_history():
+    #endpoint to get and return stored chat history
+    return JSONResponse(content={"history": chat_history}) 
